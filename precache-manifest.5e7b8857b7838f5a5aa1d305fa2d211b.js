b76ac16fdc6aa1976949d8970e1a6aca76c7819b
Subham95-git <roysubham505@gmail.com>
2020-08-29 11:09:05 +0530

    Updates

---
+++ precache-manifest.5e7b8857b7838f5a5aa1d305fa2d211b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4da874f2805c5dc1727a31bf7a2fe84d",
+    "url": "/My_new_repo/index.html"
+  },
+  {
+    "revision": "1f621f0c8a97a1b8f942",
+    "url": "/My_new_repo/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "fd4b71e3059cb79d55a1",
+    "url": "/My_new_repo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1f621f0c8a97a1b8f942",
+    "url": "/My_new_repo/static/js/2.c3e2286d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My_new_repo/static/js/2.c3e2286d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd4b71e3059cb79d55a1",
+    "url": "/My_new_repo/static/js/main.bc026769.chunk.js"
+  },
+  {
+    "revision": "cd3b63c04059dcf84526",
+    "url": "/My_new_repo/static/js/runtime-main.9d2eb604.js"
+  }
+]);
